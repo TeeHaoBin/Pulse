f9b0b008b7aa47d8ba509c8b52d2d0ef826ac473
TeeHaoBin <125014451+TeeHaoBin@users.noreply.github.com>
2026-06-02 23:58:56 +0800

    refactor: introduce CreatePatientValidationGroup for DTO constraints
    - Created CreatePatientValidationGroup marker interface to support JSR-303 validation grouping.
    - Modified PatientRequestDTO to apply the @NotBlank constraint on registeredDate only when the CreatePatientValidationGroup is active, allowing it to be optional during updates.

--- patient-service/src/main/java/com/pulse/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pulse/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.pulse.patientservice.dto;
 
+import com.pulse.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,7 +21,7 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of birth is required.")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered data is required.")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered data is required.")
     private String registeredDate;
 
     public String getName() {
---
+++ patient-service/src/main/java/com/pulse/patientservice/dto/validators/CreatePatientValidationGroup.java
@@ -0,0 +1,4 @@
+package com.pulse.patientservice.dto.validators;
+
+public interface CreatePatientValidationGroup {
+}
